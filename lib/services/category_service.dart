4bf5ae347fd5a97ec5c08cddaf0b8313635d468e
htiketun <kohtiketun@outlook.com>
2025-02-03 19:08:13 +0000

    Add screen unit tests; Improve service test coverage; Refactor project metadata; Add new calendar widget

--- lib/services/category_service.dart
+++ lib/services/category_service.dart
@@ -404,3 +404,4 @@ class CategoryService extends ChangeNotifier {
 // Commit 195: 2025-03-31T05:34:15
 // Commit 196: 2025-03-31T12:47:27
 // Commit 1: 2025-02-02T00:44:16
+// Commit 7: 2025-02-03T19:08:13
--- lib/widgets/search_filter_bar.dart
+++ lib/widgets/search_filter_bar.dart
@@ -309,3 +309,4 @@ class _SearchFilterBarState extends State<SearchFilterBar> {
 // Commit 180: 2025-03-26T20:03:31
 // Commit 199: 2025-04-01T09:57:49
 // Commit 3: 2025-02-02T14:53:29
+// Commit 7: 2025-02-03T19:08:13
