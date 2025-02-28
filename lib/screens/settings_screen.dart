14d05df18e8e1f985fe283c6162a2b319749db7b
htiketun <kohtiketun@outlook.com>
2025-02-28 20:54:52 +0000

    Improve error handling in utils; Update description field; Add new calendar widget; Update analytics screen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -748,3 +748,4 @@ class _SettingsScreenState extends State<SettingsScreen>
 // Commit 71: 2025-02-22T16:26:35
 // Commit 77: 2025-02-24T10:51:41
 // Commit 91: 2025-02-28T13:28:52
+// Commit 92: 2025-02-28T20:54:52
--- lib/widgets/recent_transactions_card.dart
+++ lib/widgets/recent_transactions_card.dart
@@ -230,3 +230,4 @@ class RecentTransactionsCard extends StatelessWidget {
 // Commit 69: 2025-02-22T02:21:26
 // Commit 76: 2025-02-24T03:16:36
 // Commit 87: 2025-02-27T09:29:48
+// Commit 92: 2025-02-28T20:54:52
