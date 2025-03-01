d78e350e3996ae012f941f4089bd40cd95dbe756
htiketun <kohtiketun@outlook.com>
2025-03-01 10:31:52 +0000

    Update author information; Add test cases for utils; Add error reporting; Fix typos in documentation

--- lib/services/category_service.dart
+++ lib/services/category_service.dart
@@ -412,3 +412,4 @@ class CategoryService extends ChangeNotifier {
 // Commit 81: 2025-02-25T15:01:07
 // Commit 86: 2025-02-27T02:35:02
 // Commit 90: 2025-02-28T06:18:29
+// Commit 94: 2025-03-01T10:31:52
