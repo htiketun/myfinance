858bcdd2d466f82dc994eef6d41ad3e7a259cec1
htiketun <kohtiketun@outlook.com>
2025-03-07 23:00:12 +0000

    Refactor budget calculation logic; Refactor string helpers; Update banner image; Refactor dashboard screen

--- lib/services/theme_service.dart
+++ lib/services/theme_service.dart
@@ -146,3 +146,4 @@ class ThemeService extends ChangeNotifier {
 // Commit 75: 2025-02-23T20:21:55
 // Commit 104: 2025-03-04T09:55:17
 // Commit 110: 2025-03-06T03:53:47
+// Commit 116: 2025-03-07T23:00:12
