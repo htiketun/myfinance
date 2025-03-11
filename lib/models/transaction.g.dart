459184cfdf6cffbf714dacb6a6d8eca1ea9dc0a6
htiketun <kohtiketun@outlook.com>
2025-03-11 18:51:18 +0000

    Improve data service logic; Add event model; Add contribution guidelines; Improve utils documentation

--- lib/models/transaction.g.dart
+++ lib/models/transaction.g.dart
@@ -201,3 +201,4 @@ class RecurringTypeAdapter extends TypeAdapter<RecurringType> {
 // Commit 173: 2025-03-24T18:43:00
 // Commit 6: 2025-02-03T11:40:25
 // Commit 36: 2025-02-12T08:46:50
+// Commit 129: 2025-03-11T18:51:18
